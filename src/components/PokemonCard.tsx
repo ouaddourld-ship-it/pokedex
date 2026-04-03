dba1bc6ec55402349d74c937db50a887f1402726
ouaddour <ouaddourld@gmail.com>
2026-04-03 02:01:49 +0200

    pokcard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,3 +1,12 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 function PokemonCard() {
     return <figure>
         <img
